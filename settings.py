b1d19a3ab53077e41eac314c35e8013f8365497d
SoLaughlin <76636657+SoLaughlin@users.noreply.github.com>
2021-01-17 03:42:17 +0000

    Create settings.py
    module containing a settings class to store all settings for alien invasion project

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for game project alien_invasion"""
+
+    def __init__(self):
+        """initialize all games settings."""
+        #screen settings
+        self.screen_width = 1920
+        self.screen_width = 1080
+        self.bg_colour = (230, 230, 230)
