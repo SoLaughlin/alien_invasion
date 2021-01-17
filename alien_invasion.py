c7d871f1db1c822f2567e3f589c6b130b5f5a89e
SoLaughlin <76636657+SoLaughlin@users.noreply.github.com>
2021-01-17 23:47:31 +0000

    added functionality to move continuously left

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,11 +48,24 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # on key down
             elif event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            # on key release
             elif event.type == pygame.KEYUP:
-                self.ship.moving_right = False
+
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
 
 
 if __name__ == '__main__':
