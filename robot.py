11069b339d29eb6980277f2d692f273f985a97f8
SoLaughlin <76636657+SoLaughlin@users.noreply.github.com>
2021-01-20 18:04:34 +0000

    Update robot.py
    updated the robot to now be a sprite and set its rects

--- robot.py
+++ robot.py
@@ -1,21 +1,25 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Robot:
+class Robot(Sprite):
     """Class to manage a robot character"""
+
     def __init__(self, ai_game):
         """initialize the robot and set its starting position"""
+        super().__init__()
         self.screen = ai_game.screen
-        # rect = rectangle
-        self.screen_rect = ai_game.screen.get_rect()
 
-        # load the ship image and get its rect.
+        # load the robot image and get its rect.
 
         self.image = pygame.image.load('images/robot_fixed.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new robot at the middle of the screen
-        self.rect.center = self.screen_rect.center
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
 
     def blitme(self):
         """Draw the ship at its current location"""
